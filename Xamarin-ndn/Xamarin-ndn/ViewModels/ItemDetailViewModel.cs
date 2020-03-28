a943d4981cf149bd054ce6b775501d527330ac77
corona <rlaehdgkr7@naver.com>
2020-03-28 14:12:45 +0900

    초기 커밋.

---
+++ Xamarin-ndn/Xamarin-ndn/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin_ndn.Models;
+
+namespace Xamarin_ndn.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
